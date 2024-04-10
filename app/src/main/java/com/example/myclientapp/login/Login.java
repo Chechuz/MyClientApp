265852cae8a803566d5d00127f82f83ac9cf9e09
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-10 12:34:59 +0200

    creacion del Shared Preferences

--- app/src/main/java/com/example/myclientapp/login/Login.java
+++ app/src/main/java/com/example/myclientapp/login/Login.java
@@ -2,15 +2,45 @@ package com.example.myclientapp.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myclientapp.MainActivity;
 import com.example.myclientapp.R;
 
 public class Login extends AppCompatActivity {
-
+    Button btnEntrar;
+    EditText etiUser, etiPass;
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnEntrar=(Button) findViewById(R.id.btn_entrar);
+        etiUser =(EditText) findViewById(R.id.eti_user);
+        etiPass =(EditText) findViewById(R.id.eti_pass);
+
+        preferences = getSharedPreferences("loginData", Context.MODE_PRIVATE);
+        etiUser.setText(preferences.getString("user", ""));
+        etiPass.setText(preferences.getString("pass", ""));
+    }
+    public void Entrar (View view){
+        preferences = getSharedPreferences("loginData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user", etiUser.getText().toString());
+        editor.putString("pass", etiPass.getText().toString());
+        editor.apply();
+
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+
+        Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show();
+
     }
 }
