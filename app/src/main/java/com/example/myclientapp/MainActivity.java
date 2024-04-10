836b89ef64fc45539076c75840164202771abd2b
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-10 16:35:02 +0200

    pongo el menu-anade-cliente en Main sin funcionalidad aun.

--- app/src/main/java/com/example/myclientapp/MainActivity.java
+++ app/src/main/java/com/example/myclientapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myclientapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_anade_cliente, menu);
+        return true;
     }
 }
