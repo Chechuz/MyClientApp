5b631f829a99a23bb914a65e43fc77271debca94
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-10 16:40:30 +0200

    añado Fragments del paquete Cliente

--- app/src/main/java/com/example/myclientapp/cliente/ClientActivity.java
+++ app/src/main/java/com/example/myclientapp/cliente/ClientActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import com.example.myclientapp.R;
 
 public class ClientActivity extends AppCompatActivity {
-
+    /**
+     *
+     Esta podria ser la "Vista del cliente?? o deberia crear un fragment para la vista
+     del Cliente con su respectivo menú.???
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
