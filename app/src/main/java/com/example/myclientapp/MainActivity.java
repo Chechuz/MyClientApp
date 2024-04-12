6feb91a08c922a5739ba3efc369f27aae74fecc6
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-12 19:41:57 +0200

    metodos para visualizar el recycler metodo para el onclic del item menu que abre "nuevo cliente"

--- app/src/main/java/com/example/myclientapp/MainActivity.java
+++ app/src/main/java/com/example/myclientapp/MainActivity.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.annotation.NonNull;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Adapter;
-
+import static com.example.myclientapp.R.*;
+import com.example.myclientapp.adapter.AdapterCliente;
 import com.example.myclientapp.bbdd.DataBase;
+import com.example.myclientapp.cliente.ClientActivity;
 import com.example.myclientapp.cliente.Cliente;
+import com.example.myclientapp.cliente.NuevoClienteFragment;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recycler;
-    Adapter adapter;
+    AdapterCliente adapter;
     List<Cliente> listaClientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recycler = findViewById(id.recycler_clientes);
+        recycler = findViewById(id.recycler_client);
         DataBase clientesDB = new DataBase(this);
         listaClientes = clientesDB.getCliente();  //llamo al List de la bbdd
 
@@ -31,12 +38,23 @@ public class MainActivity extends AppCompatActivity {
         layoutManager.setStackFromEnd(true);
 
         recycler.setLayoutManager(layoutManager);
-        adapter = new Adapter(this, listaClientes);
+        adapter = new AdapterCliente(this, listaClientes);
         recycler.setAdapter(adapter);
     }
+    // Hago visible el menu
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_anade, menu);
         return true;
+    }
+
+    //Añado el onclic en el item seleccionado (el intent en el item del menu)
+    // ponemos el intent en el item del menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.add);
+        Intent i = new Intent(MainActivity.this, NuevoClienteFragment.class);
+        startActivity(i);
+        return super.onOptionsItemSelected(item);
     }
 }
