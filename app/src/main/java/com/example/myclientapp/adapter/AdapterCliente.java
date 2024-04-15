78b3f8041744fa581038fbd6e9e9ebb0eb4c9ad1
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-15 15:43:44 +0200

    creo los metodos necesarios en el Adapter Clientes

--- app/src/main/java/com/example/myclientapp/adapter/AdapterCliente.java
+++ app/src/main/java/com/example/myclientapp/adapter/AdapterCliente.java
@@ -1,12 +1,17 @@
 package com.example.myclientapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myclientapp.R;
+import com.example.myclientapp.cliente.ClientActivity;
 import com.example.myclientapp.cliente.Cliente;
 
 import java.util.List;
@@ -22,22 +27,42 @@ public class AdapterCliente extends RecyclerView.Adapter<AdapterCliente.ViewHold
     @NonNull
     @Override
     public AdapterCliente.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.client_view,parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterCliente.ViewHolder holder, int position) {
+        String nombre = clienteModelo.get(position).getNombre();
+        String telefono = clienteModelo.get(position).getTelefono();
+
+        holder.clNombre.setText(nombre);
+        holder.clTelefono.setText(telefono);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return clienteModelo.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView clNombre, clTelefono;
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            clNombre = itemView.findViewById(R.id.clNombre);
+            clTelefono = itemView.findViewById(R.id.clTelefono);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ClientActivity.class);
+                    intent.putExtra("ID", clienteModelo.get(getAdapterPosition()).getId());
+                    v.getContext().startActivity(intent);
+
+                }
+            });
         }
     }
 }
