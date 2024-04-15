fc4d7e090d1a5b0715ffc8fa3e2028b02ad3c916
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-15 15:14:05 +0200

    añado metodo okClick para el boton guardar

--- app/src/main/java/com/example/myclientapp/cliente/NuevoClienteFragment.java
+++ app/src/main/java/com/example/myclientapp/cliente/NuevoClienteFragment.java
@@ -7,8 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
+
+import com.example.myclientapp.MainActivity;
 import com.example.myclientapp.R;
+import com.example.myclientapp.bbdd.DataBase;
 
 
 public class NuevoClienteFragment extends Fragment {
@@ -35,11 +46,36 @@ public class NuevoClienteFragment extends Fragment {
 
         }
     }
+    private Button btnguardar, btncancelar;
+    private EditText nom, dir, tel, mail, otro;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nuevo_cliente, container, false);
+        View root = inflater.inflate(R.layout.fragment_nuevo_cliente, container, false);
+        btnguardar= root.findViewById(R.id.btn_guardar);
+        btncancelar = root.findViewById(R.id.btn_cancelar);
+        nom = root.findViewById(R.id.eti_nom);
+        dir = root.findViewById(R.id.eti_dir);
+        tel = root.findViewById(R.id.eti_tel);
+        mail = root.findViewById(R.id.eti_email);
+        otro = root.findViewById(R.id.eti_otro);
+
+        btnguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cliente miCliente= new Cliente(nom.getText().toString(), dir.getText().toString(), tel.getText().toString(),mail.getText().toString(),otro.getText().toString());
+                DataBase db = new DataBase(getContext());
+                db.anadeCliente(miCliente);
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+                // aviso al usuario que la nota ha sido guardada
+                Toast.makeText(getContext(), "Nota guardada", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return root;
     }
 }
