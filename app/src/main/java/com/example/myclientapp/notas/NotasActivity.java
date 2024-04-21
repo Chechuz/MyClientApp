adc62208f5a01223b6d507bab43417d906a7088c
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-21 15:03:02 +0200

    modifico xml

--- app/src/main/java/com/example/myclientapp/notas/NotasActivity.java
+++ app/src/main/java/com/example/myclientapp/notas/NotasActivity.java
@@ -1,13 +1,26 @@
 package com.example.myclientapp.notas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myclientapp.R;
+import com.example.myclientapp.adapter.AdapterCliente;
+import com.example.myclientapp.cliente.Cliente;
+
+import java.util.List;
 
 public class NotasActivity extends AppCompatActivity {
-
+    TextView tvEmptyNotas;
+    RecyclerView recycler;
+    AdapterCliente adapter;
+    List<Cliente> listaClientes;
+    Fragment fragmentNewClient;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
