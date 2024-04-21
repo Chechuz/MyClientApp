f7fffde30622c894ab7ae200c1966bcf57aa3223
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-21 19:24:27 +0200

    pongo metodos para el Recycler de notas

--- app/src/main/java/com/example/myclientapp/notas/NotasActivity.java
+++ app/src/main/java/com/example/myclientapp/notas/NotasActivity.java
@@ -3,13 +3,17 @@ package com.example.myclientapp.notas;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.myclientapp.R;
 import com.example.myclientapp.adapter.AdapterCliente;
+import com.example.myclientapp.adapter.AdapterNotas;
+import com.example.myclientapp.bbdd.DataBase;
 import com.example.myclientapp.cliente.Cliente;
 
 import java.util.List;
@@ -17,13 +21,29 @@ import java.util.List;
 public class NotasActivity extends AppCompatActivity {
     TextView tvEmptyNotas;
     RecyclerView recycler;
-    AdapterCliente adapter;
-    List<Cliente> listaClientes;
-    Fragment fragmentNewClient;
+    AdapterNotas adapter;
+    List<Notas> listaNotas;
+    Fragment fragmentNuevaNota;
     FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas);
+        tvEmptyNotas = findViewById(R.id.tvEmptyNotas);
+        tvEmptyNotas.setVisibility(View.INVISIBLE);
+        recycler = findViewById(R.id.recycler_notas);
+        DataBase notasDB = new DataBase(this);
+        listaNotas = notasDB.getNote();  //llamo al List de la bbdd
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+
+        recycler.setLayoutManager(layoutManager);
+        adapter = new AdapterNotas(this, listaNotas);
+        recycler.setAdapter(adapter);
+        if(listaNotas.isEmpty()){
+            tvEmptyNotas.setVisibility(View.VISIBLE);
+        }
     }
 }
