f24ee6c58fc3f807bd71ce6b75980a0ad6083778
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-22 19:28:53 +0200

    metodo para añadir nota

--- app/src/main/java/com/example/myclientapp/notas/NuevaNotaFragment.java
+++ app/src/main/java/com/example/myclientapp/notas/NuevaNotaFragment.java
@@ -1,50 +1,36 @@
 package com.example.myclientapp.notas;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myclientapp.R;
+import com.example.myclientapp.bbdd.DataBase;
+import com.example.myclientapp.cliente.Cliente;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NuevaNotaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
+
 public class NuevaNotaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public NuevaNotaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NuevaNotaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NuevaNotaFragment newInstance(String param1, String param2) {
         NuevaNotaFragment fragment = new NuevaNotaFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -52,15 +38,59 @@ public class NuevaNotaFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
-
+    EditText titulo, detalle;
+    Button guardar;
+    String fecha, hora;
+    Calendar calendario;
+    Notas notaModelo;
+    int id;
+    Cliente cliente;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nueva_nota, container, false);
+        View root = inflater.inflate(R.layout.fragment_nueva_nota, container, false);
+        titulo = root.findViewById(R.id.anadeNota);
+        detalle = root.findViewById(R.id.cuerpoNota);
+        guardar = root.findViewById(R.id.btn_guardaNota);
+
+        //Para que muestre fecha y hora de la nota
+        calendario = Calendar.getInstance();
+        fecha=calendario.get(Calendar.YEAR)+"/"+ calendario.get(Calendar.MONTH)+"/"+ calendario.get(Calendar.DAY_OF_MONTH);
+        hora = pad(calendario.get(Calendar.HOUR))+":"+ pad(calendario.get(Calendar.MINUTE));
+        Log.d("calendar", "Fecha y Hora "+fecha+ "/"+hora);
+
+        //Obtengo datos del Cliente
+        id = getArguments().getInt("idCl");
+        DataBase db = new DataBase(getContext());
+        cliente = db.getClientes(id);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return root;
+    }
+
+    protected void almacenaNota(){
+        notaModelo= new Notas(titulo.getText().toString(), detalle.getText().toString(),fecha, hora);
+        DataBase db = new DataBase(getContext());
+        db.anadeNota(notaModelo, cliente);
+        // Pongo el intent para volver al main
+        Intent intent = new Intent(getContext(), NotasActivity.class);
+        startActivity(intent);
+        // aviso al usuario que la nota ha sido guardada
+        Toast.makeText(getContext(), "Nota guardada", Toast.LENGTH_SHORT).show();
+    }
+
+    //metodo para que la hora se visualice correctamente (con dos caracteres siempre)
+    public String pad(int i){
+        if(i<0)
+            return "0"+i;
+        return String.valueOf(i);
     }
 }
