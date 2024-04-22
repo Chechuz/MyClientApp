3e08eb69e3808adae424eda1ae1389bbd610c284
CeciliaFerrer <g.ceciliaferrer@gmail.com>
2024-04-22 20:51:27 +0200

    metodos para los botones

--- app/src/main/java/com/example/myclientapp/cliente/NuevoClienteFragment.java
+++ app/src/main/java/com/example/myclientapp/cliente/NuevoClienteFragment.java
@@ -16,16 +16,14 @@ import android.content.Intent;
 import com.example.myclientapp.MainActivity;
 import com.example.myclientapp.R;
 import com.example.myclientapp.bbdd.DataBase;
+import com.example.myclientapp.notas.NotasActivity;
 
 
 public class NuevoClienteFragment extends Fragment {
 
-
-
     public NuevoClienteFragment() {
         // Required empty public constructor
     }
-
 
     public static NuevoClienteFragment newInstance(String param1, String param2) {
         NuevoClienteFragment fragment = new NuevoClienteFragment();
@@ -45,13 +43,12 @@ public class NuevoClienteFragment extends Fragment {
     private Button btnguardar, btncancelar;
     private EditText nom, dir, tel, mail, otro;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_nuevo_cliente, container, false);
-        btnguardar= root.findViewById(R.id.btn_guardar);
+        btnguardar= root.findViewById(R.id.btn_guardaNota);
         btncancelar = root.findViewById(R.id.btn_cancelar);
         nom = root.findViewById(R.id.eti_nom);
         dir = root.findViewById(R.id.eti_dir);
@@ -62,15 +59,29 @@ public class NuevoClienteFragment extends Fragment {
         btnguardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Cliente miCliente= new Cliente(nom.getText().toString(), dir.getText().toString(), tel.getText().toString(),mail.getText().toString(),otro.getText().toString());
-                DataBase db = new DataBase(getContext());
-                db.anadeCliente(miCliente);
-                Intent intent = new Intent(getContext(), MainActivity.class);
-                startActivity(intent);
-                // aviso al usuario que el cliente ha sido guardado
-                Toast.makeText(getContext(), "Cliente guardado", Toast.LENGTH_SHORT).show();
+                saveCl();
+            }
+        });
+        btncancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnCancel();
             }
         });
         return root;
+    }
+
+    protected void saveCl(){
+        Cliente miCliente= new Cliente(nom.getText().toString(), dir.getText().toString(), tel.getText().toString(),mail.getText().toString(),otro.getText().toString());
+        DataBase db = new DataBase(getContext());
+        db.anadeCliente(miCliente);
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        startActivity(intent);
+        // aviso al usuario que el cliente ha sido guardado
+        Toast.makeText(getContext(), "Cliente guardado", Toast.LENGTH_SHORT).show();
+    }
+    protected void btnCancel(){
+        Intent intent = new Intent(getContext(), ClientActivity.class);
+        startActivity(intent);
     }
 }
